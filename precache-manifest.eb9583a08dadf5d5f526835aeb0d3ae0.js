dfcb211dddf023edc0746807d9db2d802a94ad98
Raisa Primerova <riyuhide@gmail.com>
2020-01-22 21:31:08 -0700

    Updates

---
+++ precache-manifest.eb9583a08dadf5d5f526835aeb0d3ae0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3054abd38e0404eab8c7030bfa535e45",
+    "url": "/Tomatillos/index.html"
+  },
+  {
+    "revision": "0d334eaee9c947a0abc1",
+    "url": "/Tomatillos/static/css/main.fc7920c5.chunk.css"
+  },
+  {
+    "revision": "5ca7bcc00f894325a153",
+    "url": "/Tomatillos/static/js/2.cdb18566.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Tomatillos/static/js/2.cdb18566.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d334eaee9c947a0abc1",
+    "url": "/Tomatillos/static/js/main.ff51d11d.chunk.js"
+  },
+  {
+    "revision": "0660e6c9503858c4bbcc",
+    "url": "/Tomatillos/static/js/runtime-main.02c9fcb2.js"
+  },
+  {
+    "revision": "37584182f2ade51508177866e69e0f5f",
+    "url": "/Tomatillos/static/media/Delphi-Lux-cinema-1-1024x682.37584182.jpg"
+  },
+  {
+    "revision": "70b57f120e2944006ff34961baf6edfb",
+    "url": "/Tomatillos/static/media/camera.70b57f12.svg"
+  },
+  {
+    "revision": "1d087161a27d51200a989d7acda69731",
+    "url": "/Tomatillos/static/media/cancel.1d087161.svg"
+  },
+  {
+    "revision": "4236a780a727c8489cc4e544656ad498",
+    "url": "/Tomatillos/static/media/delete.4236a780.svg"
+  },
+  {
+    "revision": "d16afa6fc4758de3002727fd953048a9",
+    "url": "/Tomatillos/static/media/film-reel.d16afa6f.svg"
+  }
+]);
